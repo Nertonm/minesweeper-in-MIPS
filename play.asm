605bdef13e1dbca8d5bdf2df255623f6d558e9bc
Nerton <thiago2nerton@gmail.com>
2024-01-24 21:37:25 -0300

    Solving last issue

--- play.asm
+++ play.asm
@@ -1,8 +1,17 @@
 .include "macros.asm"
 
 .globl play
-
+#a1 coluna a0 linha
 play:
-	li	$t0, 8 #Botando o numero de coisos no negoci 
-	bne	 -1, 0 +($a0 * 8 + $a1)*4
+	save_context
+	#li	$t0, 8 #Botando o numero de coisos no negoclw 	$t0
+	beg:
+	sll	$t0, $a1, 8
+	add	$t1, $t0, $a0
+	sll	$t1, $t1, 2
+	bne 	$t1, -1, lost
+	restore_context
+	j beg
+	jr $ra
+	#bne	 -1, 0 +($a0 * 8 + $a1)*4
 # your code here
