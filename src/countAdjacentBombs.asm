d3294e86e1f86f0506253e84b06f018c8284dfc6
Nerton <thiago2nerton@gmail.com>
2024-02-08 20:19:04 -0300

    Something

--- src/countAdjacentBombs.asm
+++ src/countAdjacentBombs.asm
@@ -9,42 +9,41 @@
 .eqv	count $s6
 
 countAdjacentBombs:
-
-	save_context
-	move $s6, $zero
-	move row, $a0   #  
-	move col, $a1   #  
-	move $s4, row
-	move $s5, col
-	addi $s4, $s4 1
-	addi $s5, $s5 1
+	move count, $zero
+	move row, $a0   
+	move col, $a1   
+	move auxrow, row
+	move auxcol, col
+	addi auxrow, auxrow 1
+	addi auxcol, auxcol 1
 	addi row, row -1
 	addi col, col -1
 
 	loop1:
-		beq row, $s4, endloop1		# checar se loop 1 acabou
-		addi row, row, 1 		# atribui loop 2  = 0;
+		beq 	row, auxrow, endloop1		
+		addi 	row, row, 1 		
 				
 		loop2:
-			beq col, $s4, endloop2
-			move $t0, row
-			move $t1, col
-			sll $t0, col,  5 # i*8
-			sll $t1, row, 2 # j
-  			add $t0, $t0, $t1
-  			add $t0, $t0, $s0
-			li $t3, -1
-			beq $t0, $t3, countadd
-countinue:# loop2++;
-	addi	count, count, 1
+			beq 	col, auxcol, loop2
+			blt	col, $zero, aaa 
+			bgt	col, SIZE, aaa
+			blt	row, $zero, aaa 	
+			bgt	row, SIZE, aaa
+			sll 	$t0, col,  5 	
+			sll 	$t1, row, 2 	
+  			add 	$t0, $t0, $t1
+			beq 	$t0, -1, countadd
+			aaa: 
+				addi 	col, col, 1
+				j 	loop1
 
 countadd:
 	addi 	count, count, 1
-	j 	countinue
+	addi 	col, col, 1
+	j 	loop1
 
 endloop1:
-	restore_context
-	jr 	$ra
+	j	play2
 
 endloop2:
-	addi 	row, row, 1
+	j	loop1
--- src/play.asm
+++ src/play.asm
@@ -1,21 +1,22 @@
 .include "macros.asm"
 
 .globl play
+.globl play2
 .eqv bomb
+.eqv 	row $s2 
+.eqv 	col $s3
 
 play:
 	save_context
-	move 	$s2, $a0  	#  Saving row to subroutine space
-	move 	$s3, $a1  	#  Saving row to subroutine space 
-	sll 	$t0, $s2, 5	#  Math for calculing the matrix space
-	sll 	$t1, $s3, 2	#  Math for calculing the matrix space
-	add 	$t2, $t0, $t1	#  Math for calculing the matrix space
-	
+	move 	row, $a0  		#  Saving row to subroutine space
+	move 	col, $a1  		#  Saving row to subroutine space 
+	sll 	$t0, row, 5		#  Math for calculing the matrix space
+	sll 	$t1, col, 2		#  Math for calculing the matrix space
+	add 	$t2, $t0, $t1		#  Math for calculing the matrix space
 	li 	$t3, -1	
-	move	$a3, $t2
-	
 	beq 	$t2, $t3, lost		# Actual bomb check 
-	jal	countAdjacentBombs
+	j	countAdjacentBombs
+	play2:
 	j 	continue		# If not bomb continue
 	
 	lost:			
